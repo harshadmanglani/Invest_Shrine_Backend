7b6a6cb9d1b757f4ff6fd2719dbb804f9ca44168
Harshad Manglani <harshad.gm@gmail.com>
2020-05-23 16:20:29 +0530

    entrepeneur init

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangosite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'd2',
+        'USER': 'harshad@insurance-claim',
+        'PASSWORD': 'password123$',
+        'HOST': 'insurance-claim.mysql.database.azure.com',
+        'PORT': '3306'
     }
 }
 
