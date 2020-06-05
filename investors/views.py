270aa4051ca253ad1e43e39a54cac7fffd3d6b44
Aditya Khandelwal <akaditya05@gmail.com>
2020-06-05 22:10:59 +0530

    stable

--- investors/views.py
+++ investors/views.py
@@ -3,15 +3,26 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import *
 from .forms import *
 from entrepreneurs.models import EntrepreneurPortfolioModel as Entrepreneur
-"""def register(request):
+from django.contrib.auth.decorators import login_required
 
-    form = UserCreationForm
-    context = {"form" : form}
-    
-    return render(request = request, template_name = "investors/register_investor.html",
-                    context = context)"""
+from django.contrib.auth.decorators import user_passes_test
 
 
+"""
+def group_required(*group_names):
+  #Requires user membership in at least one of the groups passed in.
+
+   def in_groups(u):
+       if u.is_authenticated:
+           if bool(u.groups.filter(name__in =group_names)) | u.is_superuser:
+               return True
+       return False
+   return user_passes_test(in_groups)"""
+
+
+
+@login_required(login_url='/login/')
+#@group_required('Investors')
 def investor_homepage(request):
     allEntrepreneurs = Entrepreneur.objects.all()
     context = {"Entrepreneurs" : allEntrepreneurs}
