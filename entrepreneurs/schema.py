7f3c4c5591c86d4190593597e2b592b695620270
Harshad Manglani <harshad.gm@gmail.com>
2020-06-18 21:40:47 +0530

    graphql fixed

--- entrepreneurs/schema.py
+++ entrepreneurs/schema.py
@@ -1,25 +1,25 @@
 from graphene_django import DjangoObjectType
 import graphene
-from .models import *
-from investors.models import * 
+from .models import PortfolioEnt as EntrepreneurPortfolioModel
+from investors.models import Portfolio as InvestorPortfolioModel
 
 class EntrepreneurPortfolio(DjangoObjectType):
     class Meta:
         model = EntrepreneurPortfolioModel
 
-class InvestorPortfolioModel(DjangoObjectType):
+class InvestorPortfolio(DjangoObjectType):
     class Meta:
         model = InvestorPortfolioModel
 
 
 class Query(graphene.ObjectType):
-    entrepreneur_portfolio = graphene.List(EntrepreneurPortfolioModel)
-    investor_portfolio = graphene.List(InvestorPortfolioModel)
+    entrepreneur_portfolio = graphene.List(EntrepreneurPortfolio)
+    investor_portfolio = graphene.List(InvestorPortfolio)
 
-    def resolve_entrepreneur_portfolio(self, info):
+    def resolve_entrepreneur_portfolio(self, context):
         return EntrepreneurPortfolioModel.objects.all()
     
-    def resolve_investor_portfolio(self, info):
+    def resolve_investor_portfolio(self, context):
         return InvestorPortfolioModel.objects.all()
 
 schema = graphene.Schema(query=Query)
