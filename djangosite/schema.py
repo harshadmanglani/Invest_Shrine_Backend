e3996a2e18e62d32a20fda3e05de0b09c5e4b628
Harshad Manglani <47568882+harshadmanglani@users.noreply.github.com>
2020-07-23 21:45:09 +0530

    Added token auth for all queries

--- djangosite/schema.py
+++ djangosite/schema.py
@@ -7,7 +7,9 @@ import investors.schema
 
 
 class Query(entrepreneurs.schema.Query,investors.schema.Query, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 class Mutation(entrepreneurs.schema.myEntMutation,investors.schema.myInvMutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
