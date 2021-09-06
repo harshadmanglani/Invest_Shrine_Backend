05196c1c4a2fbcd789a0d662650d5b9c752eab08
Harshad Manglani <47568882+harshadmanglani@users.noreply.github.com>
2021-09-06 13:52:46 +0530

    Update settings.py

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -150,9 +150,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-DEFAULT_FILE_STORAGE = 'djangosite.custom_azure.AzureMediaStorage'
-STATICFILES_STORAGE = 'djangosite.custom_azure.AzureStaticStorage'
-
 MEDIA_LOCATION = "mediaone"
 AZURE_ACCOUNT_NAME = "insurance1"
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
